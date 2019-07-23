a31e2d63c4dad4fed66df35b3676aef5709f1b28
Ardy Junata <ardyjunata68@gmail.com>
2019-07-24 03:15:52 +0700

    facebook api

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,20 +3,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    private $fb;
     public function __construct()
     {
         parent::__construct();
         $this->load->library('form_validation');
+        $this->load->library('facebooksdk');
+        $this->fb = $this->facebooksdk;
     }
 
     public function index()
     {
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
         if ($this->form_validation->run() == false) {
             $data['title'] = 'Login Page';
+            $cb = "http://localhost/CILogin/auth/callback";
+            $url = $this->fb->getLoginUrl($cb);
+            $datas = array('url' => $url);
             $this->load->view('templates/auth_header', $data);
-            $this->load->view('auth/login');
+            $this->load->view('auth/login', $datas);
             $this->load->view('templates/auth_footer');
         } else {
             //validasinya success
@@ -106,5 +113,12 @@ class Auth extends CI_Controller
     public function blocked()
     {
         $this->load->view('auth/blocked');
+    }
+
+    public function callback()
+    {
+        $act = $this->fb->getAccessToken();
+        $data = $this->fb->getUserData($act);
+        print("oke");
     }
 }
